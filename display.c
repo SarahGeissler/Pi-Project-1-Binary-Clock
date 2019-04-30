ccc67617ca4841854043f1cdda2598b5983a1c0c
SarahGeissler <43176609+SarahGeissler@users.noreply.github.com>
2019-04-30 09:18:26 -0400

    Create display.c

---
+++ display.c
@@ -0,0 +1,24 @@
+void display_time(int hours, int minutes, int seconds, pi_framebuffer_t *dev){
+
+}
+displays the time on device dev using the below helpers.
+
+void display_colons(pi_framebuffer_t *dev{
+
+}
+draws the colons
+
+void display_hours(int hours, pi_framebuffer_t *dev){
+
+}
+displays the hours on device dev as above
+
+void display_minutes(int minutes, pi_framebuffer_t *dev){
+
+}
+displays the minutes on device dev as above
+
+void display_seconds(int seconds, pi_framebuffer_t *dev){
+
+}
+displays the seconds on device dev as above
